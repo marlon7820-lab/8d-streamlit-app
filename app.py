f6e8e90fbbb73be6fbc0b0b45c8eaf4e98f20fae
marlon7820-lab <marlon7820@gmail.com>
2025-10-17 11:08:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -62,8 +62,8 @@ st.markdown("<h1 style='text-align: center; color: #1E90FF;'>📋 8D Report Assi
 # ---------------------------
 # Version info
 # ---------------------------
-version_number = "v1.0.9"
-last_updated = "October 10, 2025"
+version_number = "v1.1.0"
+last_updated = "October 17, 2025"
 st.markdown(f"""
 <hr style='border:1px solid #1E90FF; margin-top:10px; margin-bottom:5px;'>
 <p style='font-size:12px; font-style:italic; text-align:center; color:#555555;'>
