03ebd7f0cd4027d83f611bf4035e33045c0d1150
marlon7820-lab <marlon7820@gmail.com>
2025-10-29 15:09:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -968,13 +968,13 @@ line-height:1.5;
 
         
         if step == "D4":
-            st.session_state[step]["location"] = st.selectbox(
+            st.session_state[step]["location"] = st.multiselect(
                 "Location of Material",
                 ["", "Work in Progress", "Stores Stock", "Warehouse Stock", "Service Parts", "Other"],
                 index=0,
                 key="d4_location"
             )
-            st.session_state[step]["status"] = st.selectbox(
+            st.session_state[step]["status"] = st.multiselect(
                 "Status of Activities",
                 ["", "Pending", "In Progress", "Completed", "Other"],
                 index=0,
